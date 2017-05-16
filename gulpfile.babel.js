c01f75ffc10bb9ca1aa8ffa9cb31926ec4b5bf09
Justin Toh <tohjustin@hotmail.com>
2017-05-15 22:12:14 -0700

    config: Remove gulp-lint & refactor gulp config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,6 @@ import imagemin from 'gulp-imagemin';
 import sass from 'gulp-sass';
 
 // JAVASCRIPT
-import eslint from 'gulp-eslint';
 import browserify from 'browserify';
 import babelify from 'babelify';
 import source from 'vinyl-source-stream';
@@ -34,7 +33,9 @@ const paths = {
 gulp.task('html', () => {
   gulp.src(paths.html)
     .pipe(gulp.dest(bases.dist))
-    .pipe(browserSync.stream({ match: paths.dist }));
+    .pipe(browserSync.stream({
+      match: paths.dist,
+    }));
 });
 
 // Process SCSS files and concatenate them into one output file
@@ -45,7 +46,9 @@ gulp.task('styles', () => {
       outputStyle: 'compressed',
     }).on('error', sass.logError))
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
+      browsers: [
+        'last 2 versions',
+      ],
     }))
     .pipe(concat('style.min.css'))
     .pipe(minifycss())
@@ -54,33 +57,14 @@ gulp.task('styles', () => {
     .pipe(browserSync.stream({ match: paths.dist }));
 });
 
-gulp.task('lint', () => {
-  // ESLint ignores files with "node_modules" paths.
-  // So, it's best to have gulp ignore the directory as well.
-  // Also, Be sure to return the stream from the task;
-  // Otherwise, the task may end before the stream has finished.
-  gulp.src(['**/*.js', '!node_modules/**'])
-    // eslint() attaches the lint output to the "eslint" property
-    // of the file object so it can be used by other modules.
-    .pipe(eslint())
-    // eslint.format() outputs the lint results to the console.
-    // Alternatively use eslint.formatEach() (see Docs).
-    .pipe(eslint.format())
-    // To have the process exit with an error code (1) on
-    // lint error, return the stream and pipe to failAfterError last.
-    .pipe(eslint.failAfterError());
-});
-
 gulp.task('scripts', () => {
   // app.js is your main JS file with all your module inclusions
   browserify({ entries: paths.app, debug: true })
-    // .transform(babelify, { presets: ['es2015'] })
     .transform(babelify, { presets: ['es2015'], sourceMaps: true })
     .bundle()
     .pipe(source('app.min.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
-    // .pipe(uglify({ compress: false }))
     .pipe(uglify())
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest(bases.dist));
@@ -91,11 +75,13 @@ gulp.task('imagemin', () => {
   gulp.src(paths.images)
     .pipe(imagemin())
     .pipe(gulp.dest(`${bases.dist}images/`))
-    .pipe(browserSync.stream({ match: paths.dist }));
+    .pipe(browserSync.stream({
+      match: paths.dist,
+    }));
 });
 
-browserSync.create();
 gulp.task('watch', () => {
+  browserSync.create();
   browserSync.init({
     injectChanges: true,
     server: `./${bases.dist}`,
@@ -106,4 +92,4 @@ gulp.task('watch', () => {
   gulp.watch(paths.images, ['imagemin']);
 });
 
-gulp.task('serve', ['html', 'styles', 'lint', 'scripts', 'imagemin', 'watch']);
+gulp.task('serve', ['html', 'styles', 'scripts', 'imagemin', 'watch']);
