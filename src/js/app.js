e07178a0e78e2142693e839955859b128549edb6
Justin Toh <tohjustin@hotmail.com>
2017-08-25 22:09:28 -0700

    website: Add node garden animation

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import feather from 'feather-icons';
+import Garden from './garden';
 
 const ICON_SIZE = 28;
 const ICON_STROKE_WIDTH = 2;
@@ -16,5 +17,13 @@ ready(() => {
     height: `${ICON_SIZE}px`,
     'stroke-width': ICON_STROKE_WIDTH,
     width: `${ICON_SIZE}px`,
+  });
+
+  const $container = document.querySelector('.garden');
+  const g = new Garden($container);
+  g.start();
+
+  window.addEventListener('resize', () => {
+    g.resize();
   });
 });
